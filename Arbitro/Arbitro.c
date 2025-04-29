cc0bae1452611dc976167ec48561e058619a9695
Francisco Carvalho <84443911+inclyner@users.noreply.github.com>
2025-04-29 18:40:02 +0100

    [NOT TESTED] implemented getRegistryValues, getRandomLetter, and init of threads

--- Arbitro/Arbitro.c
+++ Arbitro/Arbitro.c
@@ -1,8 +1,173 @@
-// Arbitro.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Arbitro.c : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include <windows.h>
+#include <tchar.h>
+#include <io.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <winreg.h>
 
 
-int main()
-{
 
+#define TAM 200
+TCHAR szName[] = TEXT("Global\\publicNamedPipe");
+
+int _tmain(int argc, TCHAR* argv[]) {
+#ifdef UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+	_setmode(_fileno(stderr), _O_WTEXT);
+#endif
+
+	int max_letras, ritmo;
+	getRegistryValues(&max_letras, &ritmo);
+
+	TCHAR* letras_jogo = (char*)malloc(max_letras * sizeof(char));
+
+    TCHAR abecedario[26] = {
+        _T('A'), _T('B'), _T('C'), _T('D'), _T('E'), _T('F'), _T('G'), _T('H'),
+        _T('I'), _T('J'), _T('K'), _T('L'), _T('M'), _T('N'), _T('O'), _T('P'),
+        _T('Q'), _T('R'), _T('S'), _T('T'), _T('U'), _T('V'), _T('W'), _T('X'),
+        _T('Y'), _T('Z')
+    };
+
+	if (letras_jogo == NULL) {
+		_tprintf(_T("Erro a alocar memória!\n"));
+		return 1;
+	}
+	for (int i = 0; i < max_letras; i++) {
+		letras_jogo[i] = _T('_');
+	}
+
+	DWORD adminThreadId;
+	//lança thread que ouve comandos do admin
+	HANDLE hThreadAdmin = CreateThread(
+		NULL,
+		0,
+		processAdminComands,
+		NULL,
+		0,
+		&adminThreadId
+	);
+
+	if (hThreadAdmin == NULL) {
+		printf("Erro ao criar thread: %lu\n", GetLastError());
+		return 1;
+	}
+
+
+
+
+
+
+
+
+
+	WaitForSingleObject(hThreadAdmin, INFINITE);
+	// Libertar memória
+	free(letras_jogo);
+	CloseHandle(hThreadAdmin);
+
+
+	return 0;
+
+}
+
+
+
+int getRegistryValues(int* maxletras, int* ritmo) {
+	HKEY hKey;
+	DWORD tipo = REG_DWORD;
+	DWORD tam = sizeof(DWORD);
+	DWORD valMaxLetras = 6; 
+	DWORD valRitmo = 3;     
+	
+	TCHAR chave[] = _T("Software\\TrabSO2");
+
+	
+	LSTATUS res = RegOpenKeyEx(
+		HKEY_CURRENT_USER,
+		chave,
+		0,
+		KEY_ALL_ACCESS,
+		&hKey
+	);
+
+	if (res != ERROR_SUCCESS) {
+		_tprintf(_T("Chave não encontrada. A criar...\n"));
+
+		res = RegCreateKeyEx(
+			HKEY_CURRENT_USER,
+			_T("Software\\TrabSO2"),
+			0,
+			NULL,
+			REG_OPTION_NON_VOLATILE,
+			KEY_ALL_ACCESS,
+			NULL,
+			&hKey,
+			NULL
+		);
+
+		if (res != ERROR_SUCCESS) {
+			_tprintf(_T("Erro ao criar a chave!\n"));
+			return 1;
+		}
+
+		RegSetValueEx(hKey, _T("MAXLETRAS"), 0, REG_DWORD, (const BYTE*)&valMaxLetras, sizeof(DWORD));
+		RegSetValueEx(hKey, _T("RITMO"), 0, REG_DWORD, (const BYTE*)&valRitmo, sizeof(DWORD));
+	}
+	
+	tam = sizeof(DWORD);
+	res = RegQueryValueEx(hKey, _T("MAXLETRAS"), NULL, &tipo, (LPBYTE)&valMaxLetras, &tam);
+	if (res != ERROR_SUCCESS) {
+		_tprintf(_T("MAXLETRAS não existe, a criar com valor 6.\n"));
+		valMaxLetras = 6;
+		RegSetValueEx(hKey, _T("MAXLETRAS"), 0, REG_DWORD, (const BYTE*)&valMaxLetras, sizeof(DWORD));
+	}
+
+	res = RegQueryValueEx(hKey, _T("RITMO"), NULL, &tipo, (LPBYTE)&valRitmo, &tam);
+	if (res != ERROR_SUCCESS) {
+		_tprintf(_T("RITMO não existe, a criar com valor 3.\n"));
+		valRitmo = 3;
+		RegSetValueEx(hKey, _T("RITMO"), 0, REG_DWORD, (const BYTE*)&valRitmo, sizeof(DWORD));
+	}
+
+
+	if (valMaxLetras > 12) {
+		_tprintf(_T("MAXLETRAS > 12. A corrigir para 12.\n"));
+		valMaxLetras = 12;
+		RegSetValueEx(hKey, _T("MAXLETRAS"), 0, REG_DWORD, (const BYTE*)&valMaxLetras, sizeof(DWORD));
+	}
+
+	RegCloseKey(hKey);
+	*maxletras = valMaxLetras;
+	*ritmo = valRitmo;
+
+	return 0;
+}
+
+
+TCHAR* getRandomLetter(TCHAR* abecedario, int max_letras) {
+	int randomIndex = rand() % 26;
+	TCHAR* letra = (TCHAR*)malloc(sizeof(TCHAR));
+	if (letra == NULL) {
+		_tprintf(_T("Erro a alocar memória!\n"));
+		return NULL;
+	}
+	*letra = abecedario[randomIndex];
+	return letra;
+}
+
+
+DWORD WINAPI processAdminComands(LPVOID param) {
+	printf("Thread Admin a correr!\n");
+
+	return 0;
+}
+
+
+
+DWORD WINAPI admitUsers(LPVOID param) {
+	printf("Thread admitUsers a correr!\n");
+	return 0;
 }
--- Painel/Painel.c
+++ Painel/Painel.c
@@ -1,6 +1,10 @@
-// Painel.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Painel.c : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include <stdio.h>
+
+
 
 int main()
 {
+	
 }
